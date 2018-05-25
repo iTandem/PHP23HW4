94d3c38a74d06dd5344b63d9cab5325e259fd7b7
iTandem <38475030+iTandem@users.noreply.github.com>
2018-05-25 11:16:53 +0400

    Изменения по пунктам 1,2,3,5,6

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-    $city = 'Samara';
+    $city = 'Samara,ru';
+    $link = 'http://api.openweathermap.org/data/2.5/weather';
+    $appid = '7bb7c2254e638e89b5db741b79ab9f7f';
     if (isset($_GET['city']) && strlen($_GET['city']) > 3 && !is_numeric($_GET['city'])) {
         $city = $_GET['city'];
     }
-    $api_query = 'http://api.openweathermap.org/data/2.5/weather?q=' . ($city) . '&APPID=de5e807183d0471655957e6c60bc93e3';
+    $api_query = "$link?q=$city&appid=$appid";
     $api_weather_data = file_get_contents($api_query);
+    if ($api_weather_data === false) {
+        exit('Не удалось получить данные');
+    }
     $file_name = 'weather.json';
     $res_file = fopen($file_name, 'w+');
     fwrite($res_file, $api_weather_data);
@@ -13,15 +18,19 @@
     $json_data = fread($res_file, filesize($file_name));
     fclose($res_file);
     $data = json_decode($json_data);
+    if ($data === null) {
+        exit('Ошибка декодирования json');
+    }
     $mmrtst = 0.0075006375541921;
     $current_weather = $data->weather[0]->main;
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="https://yastatic.net/bootstrap/3.3.6/css/bootstrap.min.css"/>
   <link rel="stylesheet" href="css/style.css">
-  <title>Погода в Самаре</title>
+  <title>Погода</title>
 </head>
 <body>
 <div class="row <?= strtolower($current_weather) ?> bg">
@@ -31,32 +40,39 @@
       <p style="text-align: center; font-size: 1.2em; font-weight:100"><?= date('d/M/Y') ?></p> <br>
       <div>
         <div class="curr">
-          <img class="weather-pic" src="http://openweathermap.org/img/w/<?= $data->weather[0]->icon ?>.png">
+          <img class="weather-pic" src="http://openweathermap.org/img/w/<?= $data->weather[0]->icon ?>.png" alt="Погода">
           <h4 class="weather-condition"><?= ucfirst($data->weather[0]->description) ?></h4>
         </div>
         <div class="max-min-temp">
-          <p>&#8593; <?= round($data->main->temp_max - 273) ?> </p>
-          <p>&#8595; <?= round($data->main->temp_min - 273) ?> </p>
+          <p>
+            &#8593; <?= (!empty($data->main->temp_max)) ? round($data->main->temp_max - 273) : 'не удалось получить данные'; ?> </p>
+          <p>
+            &#8595; <?= (!empty($data->main->temp_min)) ? round($data->main->temp_min - 273) : 'не удалось получить данные'; ?> </p>
         </div>
-        <p class="current-temp"><?= round($data->main->temp - 273) ?>&#176;</p>
+        <p
+          class="current-temp"><?= (!empty($data->main->temp)) ? round($data->main->temp - 273) : 'не удалось получить данные'; ?>
+          &#176;</p>
         <div class="parameters">
           <div class="row">
             <div class="param-name">Видимость</div>
-            <div class="param-data"><?= @ round($data->visibility / 1000) ?> км</div>
+            <div
+              class="param-data"><?= (!empty($data->visibility)) ? @ round($data->visibility / 1000) : 'не удалось получить данные'; ?>
+              км
+            </div>
             <br>
           </div>
           <div class="row">
             <div class="param-name">Влажность</div>
-            <div class="param-data"><?= $data->main->humidity ?>%</div>
+            <div class="param-data"><?= (!empty($data->main->humidity)) ? $data->main->humidity : 'не удалось получить данные'; ?>%</div>
             <br>
           </div>
           <div class="row">
             <div class="param-name">Давление</div>
-            <div class="param-data"><?= round($data->main->pressure * 100 * $mmrtst) ?> мм</div>
+            <div class="param-data"><?= (!empty($data->main->pressure)) ? round($data->main->pressure * 100 * $mmrtst) : 'не удалось получить данные'; ?> мм</div>
           </div>
           <div class="row">
             <div class="param-name">Скорость ветра</div>
-            <div class="param-data"><?= $data->wind->speed ?> м/с</div>
+            <div class="param-data"><?= (!empty($data->wind->speed)) ? $data->wind->speed : 'не удалось получить данные'; ?> м/с</div>
             <br>
           </div>
         </div>
@@ -66,10 +82,10 @@
       <form class="form-inline">
         <div class="form-group">
           <div class="input-group">
-            <input class="form-control" type="text" formmethod="GET" name="city" placeholder="City" size="50"
+            <input class="form-control" type="text" formmethod="GET" name="city" placeholder="Город" size="50"
                    formenctype="text/plain">
             <div class="input-group-addon button">
-              <button class="btn btn-primary" type="submit">Submit</button>
+              <button class="btn btn-primary" type="submit">Выбрать</button>
             </div>
           </div>
         </div>
